e61fcf1a9e237d9430ab021296a410e466aa1c41
YagiRyu <lxam2cctzwcn@gmail.com>
2022-03-07 15:01:42 +0900

    Add 3 fragment: HomeFragment, ProfileFragment, GetSkillFragment

--- Andocchi/app/src/main/java/com/github/ryu/andocchi/MainActivity.kt
+++ Andocchi/app/src/main/java/com/github/ryu/andocchi/MainActivity.kt
@@ -2,10 +2,15 @@ package com.github.ryu.andocchi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.ryu.andocchi.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, HomeFragment())
+            .commit()
     }
 }
